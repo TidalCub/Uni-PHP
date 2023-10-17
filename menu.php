fbca56b1a6c1b7b8cf51a9624ce0f0a2c00cb8ad
Leon Skinner <Leon.skinner@bt.com>
2023-10-17 09:52:33 +0100

    remove unused html files

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Website</title>
+  <?php include 'views/shared/_head.php'; ?>
+</head>
+<body>
+<?php include 'views/shared/_header.php'; ?>
+<div class="items">
+  <h1>Hello</h1>
+</div>
+
+  <?php include 'views/shared/_footer.php'; ?>
+</body>
+</html>

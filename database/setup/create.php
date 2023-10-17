b0a1559b7ae808cc026535de1cc659ffcfcf967f
Leon Skinner <Leon.skinner@bt.com>
2023-10-17 10:14:57 +0100

    databses

---
+++ database/setup/create.php
@@ -0,0 +1,19 @@
+<?php 
+  $server = "localhost";
+  $username = "root";
+  $password = "password";
+  $port = 3306;
+
+  $conn = new mysqli($server, $username, $password, null, $port);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+
+  $sql = "CREATE DATABASE IF NOT EXISTS alpaca_peruleon_cafe";
+  if ($conn->query($sql) === FALSE) {
+    echo "Error creating database: " . $conn->error;
+  }
+  $conn->close();
+?>
---
+++ database/setup/create_tables.php
@@ -0,0 +1,33 @@
+<?php
+require 'database/connect.php';
+
+// Create the categories table
+$sql = "CREATE TABLE categories (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table categories created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+// Create the products table
+$sql = "CREATE TABLE products (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  description VARCHAR(255),
+  price DECIMAL(10,2) NOT NULL,
+  category_id INT(6) UNSIGNED,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table products created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>

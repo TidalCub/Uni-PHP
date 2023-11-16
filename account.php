49d13b2a351fd08760cb94224b12f6fea4fd7a10
Leon Skinner <Leon.skinner@bt.com>
2023-11-16 15:53:31 +0000

    add account page

--- account.php
+++ account.php
@@ -1,19 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require "views/shared/_head.php" ?>
+  <?php require "views/shared/_head.php" ?>
 </head>
 <body>
-    <?php require "views/shared/_header.php" ?>
-    <?php 
-        require "database/query.php";
-        $result = get_user($_SESSION["user"]);
-        while($row = $result->fetch_assoc()){
-            echo $row["email"];
-        }
+  <?php 
+    require "views/shared/_header.php";
+    require_once "user/user_obj.php";  
+    require "helpers/expand_order.php";
+    $user = new user();
     ?>
-    
-    <a href="/user/logout.php" class="btn btn-primary">Logout</a>
+  <div class="d-flex flex-wrap">
+    <div class="border-end col-12 col-md-3 col-lg-3 d-flex justify-content-center flex-column">
+      <div class="col-11 rd-normal bg-light m-3 p-3">
+        <?php foreach($user->get_user_details() as $details) : ?>
+          <h1 class="text-center">Hi <?= $details["first_name"]?></h1>
+          <hr/>
+          <div class="ps-2">
+            <table>
+              <tr>
+                <td>Name: </td>
+                <td><?= $details["first_name"] . " " . $details["last_name"]?></td>
+              </tr>
+              <tr>
+                <td>Email: </td>
+                <td><?= $user->user_email_safe($details["email"]) ?></td>
+              </tr>
+              <tr>
+                <td>Joined: </td>
+                <td><?= $details["created_at"] ?></td>
+              </tr>
+              <tr>
+                <td>Total Orders: </td>
+                <td><?= $user->total_orders()?></td>
+              </tr>
+            </table>
+            <hr/>
+              <div class="col-12 d-flex justify-content-center gap-3">
+                <a href="user/logout.php" class="btn btn-dark">Logout</a>
+                <a href="" class="btn btn-dark">Update Details</a>
+              </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="col-11 bg-light m-3 p-3 rd-normal">
+        <h4 class="text-center">Useful Resources</h4>
+        <hr/>
+      </div>
+      
+    </div>
+    <div class="col-12 col-md-9 col-lg-9 ">
+      <h1 class="text-center">Past Orders</h1>
+      <div class="col-12 d-flex gap-2 flex-wrap flex-row p-2">
+        <?php foreach($user->get_all_orders() as $order):?>
+          <div class="col-12 col-md-3 col-lg-3 bg-light rd-normal p-2">
+            <h3 class="text-center">Order: <?= $order["id"]?></h3>
+            <p class="text-center">Ordered at: <?= $order["created_at"] ?></p>
+            <hr/>
+            <?php foreach(expand_order($order["id"]) as $items) :?>
+              <p><?= $items["product_name"] ?></p>
+            <?php endforeach ?>
+            <hr/>
+            <h6 class="text-end">Total: <?=order_total($order["id"]) ?></h6>
+          </div>
+        <?php endforeach ?>
+      </div>
+      
+    </div>
+  </div>
 </body>
 
 </html>
---
+++ helpers/expand_order.php
@@ -0,0 +1,33 @@
+<?php 
+  
+  function expand_order($order_id){
+    require "database/connect.php";
+    $stmt = $conn->prepare(
+      "SELECT order_items.id, order_items.order_id, order_items.product_id, products.name AS product_name, products.description AS product_description, products.price AS product_price
+      FROM order_items
+      JOIN products ON order_items.product_id = products.id
+      WHERE order_items.order_id = ?;"
+    );
+    $stmt->bind_param("i", $order_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result;
+  }
+
+  function order_total($order_id){
+    require "database/connect.php";
+
+    $stmt = $conn->prepare(
+      "SELECT SUM(products.price) AS total
+      FROM order_items
+      JOIN products ON order_items.product_id = products.id
+      WHERE order_items.order_id = ?;"
+    );
+    $stmt->bind_param("i", $order_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $totalData = $result->fetch_assoc();
+    $stmt->close();
+    return $totalData["total"];
+  }
+?>

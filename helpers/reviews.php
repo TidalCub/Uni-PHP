38e0d84c9d927384cafa48b7de92ab1ce376176d
TidalCub <Leon.skinner@hotmail.co.uk>
2023-11-22 20:32:08 +0000

    add comments to helper functions

--- helpers/reviews.php
+++ helpers/reviews.php
@@ -1,3 +1,12 @@
+<?php
+/*
+This helper function takes a product ID as an argument, connects to the database, 
+and prepares a SQL statement to select all reviews for the product, along with the first name of the user 
+who wrote each review. It then executes the statement and fetches the result. The result, which is a list of 
+all reviews for the product, is returned by the function. Only the first name is selected from the users table 
+to ensure privacy and that no other user details are exposed.
+*/
+?>
 <?php 
   
   function get_all_reviews($product){
